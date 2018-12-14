c078586afd4cc65d2960247ad498d0fe2d791d78
Muhammad Omar <muhammad.omar.eg@gmail.com>
2018-12-15 00:22:17 +0200

    update on index method

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,11 +2,14 @@ module Api
   module V1
     class ProductsController < ApplicationController
       def index
-        vars = request.query_parameters
-        name = vars['name']
-        product =  Product.find_or_initialize_by(product_name: name)
-        render json: {status: 'SUCCESS', message:'Product Found', data:product},status: :ok
-
+        url = request.query_parameters
+        name = url['name']
+        product =  Product.where(product_name: name)
+        if product[0] != nil 
+          render json: {status: 'SUCCESS', message:'Product Found', data:product},status: :ok
+        else
+          render json: {status: 'FAILED', message:'Product Not Found'},status: :not_found
+        end
       end
     end
   end
